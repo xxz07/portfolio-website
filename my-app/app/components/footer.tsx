a77d51a5f88ececc148593f846e140903c776c64
Dennis Xu <302056276@student.rocmondriaan.nl>
2026-06-17 09:18:39 +0200

    updated the whole project.
    added a footer and a nav bar

---
+++ my-app/app/components/footer.tsx
@@ -0,0 +1,21 @@
+export default function Footer() {
+  return (
+    <footer className="mt-10 pt-1 pb-1 w-full bg-gray-300 dark:border-black sticky">
+        
+  <div className="footer-content flex flex-col my-6 items-center justify-center gap-4 text-center">
+
+    <nav className="footer-links">
+      <a href="/about"> About</a>
+      <a href="/projects"> Projects</a>
+      <a href="/skills"> Skills</a>
+    </nav>
+
+    <div className="footer-socials">
+      <a href="https://www.linkedin.com/in/xinzhe-xu-9212b2364?utm_source=share_via&utm_content=profile&utm_medium=member_ios" target="_blank" rel="noopener noreferrer"> LinkedIn</a>
+      <a href="https://github.com/xxz07" target="_blank" rel="noopener noreferrer"> GitHub</a>
+    </div>
+    <p>© 2026 Portfolio - Xinzhe Xu.</p>
+  </div>
+</footer>
+  );
+}
---
+++ my-app/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Footer from "./components/footer";
+
+
+export default function Home() {
+  return (
+    <>
+    <div className="flex flex-col flex-1 items-center justify-center font-sans">
+      <div className="bg-white">
+        hi
+      </div>
+      
+    </div>
+    <Footer />
+    </>
+    
+  );
+}
